4a7e7df90f6656356ee4b942a05f68f995adf558
Gokalp Ozer <gokalpozer59@gmail.com>
2024-12-17 02:59:59 +0300

    customize intents

--- src/insert.sql
+++ src/insert.sql
@@ -2,7 +2,7 @@ CREATE TABLE logs (
     id SERIAL PRIMARY KEY,
     conversation_id INT,
     speaker VARCHAR(30),
-    sentiment VARCHAR(30),
-    intention VARCHAR(30),
+    sentiment VARCHAR(50),
+    intention VARCHAR(50),
     message VARCHAR(200)
 );
